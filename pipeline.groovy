824bde332b89ab04ee264f93c9d6058480676249
SupunTJ <jayaweeraspn@gmail.com>
2024-06-29 01:49:10 +0530

    update pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -22,10 +22,10 @@ pipeline {
             steps {
                 script {
                     // Stop any running containers with the same name
-                    sh 'docker stop server-container || true'
-                    sh 'docker rm server-container || true'
+                    sh 'docker stop supun3998-server-app-image || true'
+                    sh 'docker rm supun3998-server-app-image || true'
                     // Run the new container
-                    sh 'docker run -d -p 3000:3000 --name server-container supun3998/server-app-image'
+                    sh 'docker run -d -p 3000:3000 --name supun3998-server-app-image supun3998/server-app-image'
                 }
             }
         }
@@ -64,59 +64,3 @@ pipeline {
     }
 }
 
-
-// pipeline {
-//     agent any
-
-//     environment {
-//         DOCKER_IMAGE = "Supun3998/server-app-image"
-//         DOCKER_REGISTRY_CREDENTIALS = 'your-docker-credentials-id' // If pushing to a private registry
-//     }
-
-//     stages {
-//         stage('SCM checkout') {
-//             steps {
-//                 retry(3) {
-//                     checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/SupunTJ/MERN-app-backend-dockerizing.git']])
-//                 }
-//             }
-//         }
-//         stage('Build Docker Image') {
-//             steps {
-//                 script {
-//                     sh "docker build -t ${DOCKER_IMAGE} ."
-//                 }
-//             }
-//         }
-//         stage('Run Docker Container') {
-//             steps {
-//                 script {
-//                     // Stop and remove any running containers with the same image
-//                     try {
-//                         sh 'docker stop $(docker ps -q --filter ancestor=${DOCKER_IMAGE})'
-//                     } catch (Exception e) {
-//                         echo 'No containers to stop'
-//                     }
-//                     try {
-//                         sh 'docker rm $(docker ps -aq --filter ancestor=${DOCKER_IMAGE})'
-//                     } catch (Exception e) {
-//                         echo 'No containers to remove'
-//                     }
-//                     // Run the new container
-//                     sh "docker run -d -p 3000:3000 ${DOCKER_IMAGE}"
-//                 }
-//             }
-//         }
-//         stage('Show Running Containers') {
-//             steps {
-//                 sh 'docker ps'
-//             }
-//         }
-//     }
-
-//     post {
-//         always {
-//             cleanWs()
-//         }
-//     }
-// }
